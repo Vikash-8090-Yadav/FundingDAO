72efd49410b24270ca42475ad178353213ca05fa
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-10-30 01:51:12 +0530

    Initialize subgraph

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({ path: './.env.local' });
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
+
+const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY
+
+module.exports = {
+  solidity: "0.8.10",
+  defaultNetwork: "polygon",
+  networks: {
+    hardhat: {},
+    polygon: {
+      url : "https://polygon-mumbai.infura.io/v3/95688893704a4d5bac083296c3547383",
+      accounts: ["4b37e644ab78c477cf92ed880dd52d5b0d50bfe36056696d1e05ba480d5abaa3"]
+    }
+  }
+};
+
+
+// 0xB38DfdACe3Fc2f70c89016131792a2aDad1d9940
+
